979c4f64abeee2332327b5c93b1055f52cc457a3
Lya Roux <lya.roux@pm.me>
2024-04-22 19:22:35 +0100

    added sessions and minor tweaks

--- script.js
+++ script.js
@@ -10,23 +10,33 @@ RUBIK'S CUBE TIMER BY MYSTYXX*/
 
 // get back data from localStorage or init an empty array if empty or invalid
 let timesStr = localStorage.getItem("times");
-let times = [];
+let times = [[]];
 
 if (timesStr) {
     try {
         times = JSON.parse(timesStr);
     } catch (error) {
         console.error("Erreur lors de l'analyse des données JSON:", error);
-        times = [];
+        times = [[]];
     }
 }
-
 let a = 0;
 let testIsReady = false;
 let testIsRunning = false;
 let timerIntervalId;
+let sessionSelect = document.getElementById("sessionSelect");
 document.getElementById("scramble").innerText = scramble(20);
 
+
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
 
 function scramble(length) {
     const moves = ['F', 'B', 'R', 'L', 'U', 'D']; // Rubik's cube sides
@@ -58,28 +68,53 @@ function timer() {
     console.log(testIsReady);
 }
 
+// function showTimes(timesIndex) {
+//     for(let i = 0; i>times[timesIndex])
+// }
+
+//add selection option for the current session
+for(let i = 1; i<=times.length; i++) {
+    let temp = create(`<option value=\"${i}\">${i}</option>`);
+    sessionSelect.appendChild(temp);
+}
+sessionSelect.value = times.length.toString();
+
+document.getElementById("sessionSelect").addEventListener("change", (createNewSession) => {
+    console.log(createNewSession.target.value)
+    if (createNewSession.target.value === "new session") {
+        times.push([]);
+        localStorage.setItem("times", JSON.stringify(times));
+        sessionSelect.appendChild(create(`<option value=${times.length}>${times.length}</option>`));
+        sessionSelect.value = times.length.toString();
+    }
+});
+
 addEventListener("keydown", (timerReady) => {
     if (timerReady.keyCode == 32) {
         if (testIsRunning) {
             // stop the test
-            testIsRunning = false;
             clearInterval(timerIntervalId);
+            testIsRunning = false;
+            
             // add current times to global times list
-            times.push(document.getElementById("currentTime").innerText);
+            times[sessionSelect.options[sessionSelect.selectedIndex].value-1].push(document.getElementById("currentTime").innerText);
             localStorage.setItem("times", JSON.stringify(times));
             document.getElementById("scramble").innerText = scramble(20);
             // alert(JSON.stringify(times)); // Afficher les temps enregistrés
         }
         a += 1;
-        if (a >= 26) {
+        if (a >= 15) {
             testIsReady = true;
+            document.getElementById("currentTime").style.color = 'green';
         }
+        else{if(!testIsRunning){document.getElementById("currentTime").style.color = 'red';}}
     }
 });
 
 addEventListener("keyup", (timerStart) => {
     if (timerStart.keyCode == 32) {
         a = 0;
+        document.getElementById("currentTime").style.color = 'black';
         if (testIsReady) {
             document.getElementById("currentTime").innerText = '0';
             testIsReady = false;
